f146dd75bb2e8270265ca565e7cc2a2cc5f203f1
sayarkhant001 <sayarkhant001@gmail.com>
2026-09-03 17:39:55 +0630

    fix: align kotlin 2.3.5 with ksp 2.3.5, fix serialization plugin version, improve CI logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesS
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
-  kotlin("plugin.serialization") version "2.0.21"
+  kotlin("plugin.serialization") version "2.3.5"
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
